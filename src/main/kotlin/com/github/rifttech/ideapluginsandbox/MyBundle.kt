2f06079b685b75d172452c9f262acbeadae34920
GitHub Action <action@github.com>
2021-02-27 14:39:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/rifttech/ideapluginsandbox/MyBundle.kt
+++ src/main/kotlin/com/github/rifttech/ideapluginsandbox/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.rifttech.ideapluginsandbox
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/rifttech/ideapluginsandbox/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rifttech.ideapluginsandbox.services
+
+import com.github.rifttech.ideapluginsandbox.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
